f8801a8b845f3ac477e55f50c90c56ad7753e27b
Airat <mingaleev.1993@mail.ru>
2023-09-09 22:19:39 +0300

    Hw 1.1

--- homework/homework1/app.py
+++ homework/homework1/app.py
@@ -8,30 +8,29 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/contact/')
-# def contact():
-#     return render_template('contact.html')
-#
-#
-# @app.route('/catalog/')
-# def catalog():
-#     _catalog = [
-#         {
-#             "name": "Одежда",
-#             "link": "cloth.html"
-#         },
-#         {
-#             "name": "Обувь",
-#             "link": "cloth.html"
-#         },
-#         {
-#             "name": "Куртки",
-#             "link": "cloth.html"
-#         }
-#     ]
-#     context = {'catalog': _catalog}
-#     return render_template('catalog.html', **context)
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/catalog/')
+def catalog():
+    context = [
+        {
+            "name": "Одежда",
+            "link": "cloth"
+        },
+        {
+            "name": "Обувь",
+            "link": "cloth"
+         },
+        {
+            "name": "Куртки",
+            "link": "cloth"
+        }
+    ]
+    return render_template('catalog.html', context=context)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('')
